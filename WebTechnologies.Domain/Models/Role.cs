9e63c5ebcdd0c45a2faea0ea048f92a57c40d4df
pxmiu <pmnsplay@gmail.com>
2023-10-06 12:35:51 +0300

    Created models

---
+++ WebTechnologies.Domain/Models/Role.cs
@@ -0,0 +1,26 @@
+﻿namespace WebTechnologies.Domain.Models;
+public class Role
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public List<User> Users { get; set; } = new();
+
+    private Role() // EF needed
+    {
+    }
+
+    public Role(string name)
+    {
+        Name = name;
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Role role && role.Name == role.Name;
+    }
+}
