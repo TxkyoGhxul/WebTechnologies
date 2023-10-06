37bc37e602c3d5b3a5a900bcd7e7ed8c79413f49
pxmiu <pmnsplay@gmail.com>
2023-10-06 12:42:45 +0300

    Created interfaces for DbContext

---
+++ WebTechnologies.Application/Interfaces/IUserDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebTechnologies.Domain.Models;
+
+namespace WebTechnologies.Application.Interfaces;
+public interface IUserDbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+}
