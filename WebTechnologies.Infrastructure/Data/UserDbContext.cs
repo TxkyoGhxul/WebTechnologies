ec67fd35c5ff4abf5510035e6178dafbefcf3b5d
pxmiu <pmnsplay@gmail.com>
2023-10-06 12:42:56 +0300

    Created DbContext

---
+++ WebTechnologies.Infrastructure/Data/UserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebTechnologies.Application.Interfaces;
+using WebTechnologies.Domain.Models;
+
+namespace WebTechnologies.Infrastructure.Data;
+internal class UserDbContext : DbContext, IUserDbContext, IUnitOfWork
+{
+    public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+}
