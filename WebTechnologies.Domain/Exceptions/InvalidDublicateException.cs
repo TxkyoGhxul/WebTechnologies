6a3e163dc63fd50c84454efd004cc5f5eb2e7022
pxmiu <pmnsplay@gmail.com>
2023-10-06 12:36:08 +0300

    Added exceptions for email

---
+++ WebTechnologies.Domain/Exceptions/InvalidDublicateException.cs
@@ -0,0 +1,15 @@
+﻿namespace WebTechnologies.Domain.Exceptions;
+
+public class InvalidDublicateException : Exception
+{
+    public InvalidDublicateException() : base("Email is already exists")
+    {
+    }
+
+    public InvalidDublicateException(string email) : base($"Email is already exists. Current value: {email}")
+    {
+        Email = email;
+    }
+
+    public string Email { get; init; }
+}
