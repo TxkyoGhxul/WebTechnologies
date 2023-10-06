f3010190f2fa92b8f4ebecd4c86c70168d8bb322
pxmiu <pmnsplay@gmail.com>
2023-10-06 13:41:19 +0300

    Added Db configurations

---
+++ WebTechnologies.Infrastructure/Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebTechnologies.Domain.Models;
+using WebTechnologies.Domain.ValueObjects;
+
+namespace WebTechnologies.Infrastructure.Data.Configurations;
+internal class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(b => b.Id);
+
+        builder.Property(b => b.Name).IsRequired().HasMaxLength(25);
+
+        builder.Property(b => b.Email).HasConversion(
+            email => email.Value,
+            value => Email.From(value));
+
+        builder.Property(b => b.BirthDate).HasConversion(
+            dateOnly => dateOnly.ToDateTime(TimeOnly.MinValue),
+            dateTime => DateOnly.FromDateTime(dateTime));
+    }
+}
