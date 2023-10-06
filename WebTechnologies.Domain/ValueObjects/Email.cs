47fe415c634ba36a3f2ba7b79a5a5aa359cbe199
pxmiu <pmnsplay@gmail.com>
2023-10-06 12:36:31 +0300

    Added value obj of email

---
+++ WebTechnologies.Domain/ValueObjects/Email.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+using ValueOf;
+using WebTechnologies.Domain.Exceptions;
+
+namespace WebTechnologies.Domain.ValueObjects;
+public class Email : ValueOf<string, Email>
+{
+    private const string _emailPattern = "^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$";
+    
+    private static readonly Regex _emailRegex = new(_emailPattern);
+
+    protected override void Validate()
+    {
+        var match = _emailRegex.Match(Value);
+        if (!match.Success)
+        {
+            throw new InvalidEmailException(Value);
+        }
+    }
+
+    protected override bool TryValidate()
+    {
+        try
+        {
+            Validate();
+            return true;
+        }
+        catch (InvalidEmailException)
+        {
+            return false;
+        }
+    }
+
+    public static implicit operator string(Email email) => email.Value;
+
+    public static explicit operator Email(string email) => From(email);
+}
