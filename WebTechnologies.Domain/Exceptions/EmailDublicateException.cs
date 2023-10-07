c54367f13b1720c83a8cdc998d4bb1ecfabb6ed2
pxmiu <pmnsplay@gmail.com>
2023-10-08 01:23:55 +0300

    Created custom exceptions

---
+++ WebTechnologies.Domain/Exceptions/EmailDublicateException.cs
@@ -0,0 +1,15 @@
+﻿namespace WebTechnologies.Domain.Exceptions;
+
+public class EmailDublicateException : Exception
+{
+    public EmailDublicateException() : base("Email is already exists")
+    {
+    }
+
+    public EmailDublicateException(string email) : base($"Email is already exists. Current value: {email}")
+    {
+        Email = email;
+    }
+
+    public string Email { get; init; }
+}
---
+++ WebTechnologies.Domain/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,22 @@
+﻿namespace WebTechnologies.Domain.Exceptions;
+public class EntityNotFoundException : Exception
+{
+    public EntityNotFoundException() : base("Entity not found")
+    {
+    }
+
+    public EntityNotFoundException(Guid id) : base($"Entity with ID: {id} not found")
+    {
+        Id = id;
+    }
+
+    public EntityNotFoundException(Guid id, Type entityType) : base($"Entity {entityType.Name} with ID: {id} not found")
+    {
+        Id = id;
+        EntityType = entityType;
+    }
+
+    public Guid Id { get; init; }
+
+    public Type EntityType { get; init; }
+}
