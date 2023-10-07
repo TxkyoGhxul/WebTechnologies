96c75827d21c5b9727804091054ac62ca16121cb
pxmiu <pmnsplay@gmail.com>
2023-10-07 14:04:33 +0300

    Added db connection

--- WebTechnologies.WebAPI/Program.cs
+++ WebTechnologies.WebAPI/Program.cs
@@ -1,11 +1,19 @@
+using WebTechnologies.Infrastructure.Extensions;
+using WebTechnologies.Presentation.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.InjectDbContext(builder.Configuration);
+
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(UserController).Assembly)
+    .AddControllersAsServices();
 
 var app = builder.Build();
 
