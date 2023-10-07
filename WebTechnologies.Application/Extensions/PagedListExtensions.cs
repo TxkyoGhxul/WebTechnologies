c509becf8bc21355d094b49938e0961a88104d11
pxmiu <pmnsplay@gmail.com>
2023-10-07 14:05:26 +0300

    Added paged list extensions

---
+++ WebTechnologies.Application/Extensions/PagedListExtensions.cs
@@ -0,0 +1,18 @@
+﻿using WebTechnologies.Application.Models;
+
+namespace WebTechnologies.Application.Extensions;
+public static class PagedListsExtensions
+{
+    public static async Task<PagedList<T>> ToPagedListAsync<T>(this IQueryable<T> values, int pageNumber, int pageSize, CancellationToken cancellationToken = default)
+    {
+        return await PagedList<T>.CreateAsync(values, pageNumber, pageSize, cancellationToken);
+    }
+
+    public static async Task<PagedList<T>> ToPagedListAsync<T>(this IQueryable<T> values, CancellationToken cancellationToken = default)
+    {
+        const int defaultPageNumber = 1;
+        const int defaultPageSize = 10;
+
+        return await values.ToPagedListAsync(defaultPageNumber, defaultPageSize, cancellationToken);
+    }
+}
