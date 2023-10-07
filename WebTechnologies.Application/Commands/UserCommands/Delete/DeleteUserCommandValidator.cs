bf9a07b70132d78aa2f41917d0490ac3fbba1603
pxmiu <pmnsplay@gmail.com>
2023-10-08 01:22:20 +0300

    Implemented delete user command

---
+++ WebTechnologies.Application/Commands/UserCommands/Delete/DeleteUserCommandValidator.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+
+namespace WebTechnologies.Application.Commands.UserCommands.Delete;
+internal class DeleteUserCommandValidator : AbstractValidator<DeleteUserCommand>
+{
+    public DeleteUserCommandValidator()
+    {
+        RuleFor(c => c.UserId).NotEmpty();
+    }
+}
