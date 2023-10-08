1cc09a7704a217bba1d229bc15ea695f04872721
pxmiu <pmnsplay@gmail.com>
2023-10-09 01:15:47 +0300

    Added options

---
+++ WebTechnologies.WebAPI/OptionsSetup/JwtOptionsSetup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Options;
+using WebTechnologies.Application.Authentication;
+
+namespace WebTechnologies.WebAPI.OptionsSetup;
+
+public class JwtOptionsSetup : IConfigureOptions<JwtOptions>
+{
+    private const string _sectionName = "Jwt";
+    private readonly IConfiguration _configuration;
+
+    public JwtOptionsSetup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void Configure(JwtOptions options)
+    {
+        _configuration.GetSection(_sectionName).Bind(options);
+    }
+}
