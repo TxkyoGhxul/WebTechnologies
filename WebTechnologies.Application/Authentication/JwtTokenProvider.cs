17dd6dd9e1009cc515badccf1174059cb37f6b25
pxmiu <pmnsplay@gmail.com>
2023-10-09 01:13:48 +0300

    Added auth service

---
+++ WebTechnologies.Application/Authentication/JwtTokenProvider.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebTechnologies.Application.Interfaces;
+using WebTechnologies.Domain.Models;
+
+namespace WebTechnologies.Application.Authentication;
+public class JwtTokenProvider : IJwtTokenProvider
+{
+    private readonly JwtOptions _jwtOptions;
+
+    public JwtTokenProvider(IOptions<JwtOptions> jwtOptions)
+    {
+        _jwtOptions = jwtOptions.Value;
+    }
+
+    public string GetJwtToken(User user)
+    {
+        var claims = new List<Claim>()
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+        };
+
+        foreach (var role in user.Roles)
+        {
+            claims.Add(new Claim(ClaimTypes.Role, role.Name));
+        }
+
+        var signingCredentinals = new SigningCredentials(
+            new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_jwtOptions.SecretKey)),
+            SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(_jwtOptions.Issuer,
+            _jwtOptions.Audience,
+            claims,
+            null,
+            DateTime.UtcNow.AddHours(1),
+            signingCredentinals);
+
+        var tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return tokenValue;
+    }
+}
