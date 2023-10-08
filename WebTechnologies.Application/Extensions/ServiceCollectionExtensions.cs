e776f52ec4fc351fbfb049096ab8de789f3f4e43
pxmiu <pmnsplay@gmail.com>
2023-10-08 16:03:53 +0300

    Injected Application services

---
+++ WebTechnologies.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,46 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using WebTechnologies.Application.Behaviors;
+using WebTechnologies.Application.Sorters;
+using WebTechnologies.Application.Sorters.Base;
+using WebTechnologies.Application.Sorters.Fields;
+using WebTechnologies.Domain.Models;
+
+namespace WebTechnologies.Application.Extensions;
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection InjectMediatR(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(PerfomanceBehavior<,>));
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ExceptionHandlingBehavior<,>));
+
+        return services;
+    }
+
+    public static IServiceCollection InjectValidators(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+
+    public static IServiceCollection InjectSorters(this IServiceCollection services)
+    {
+        services.AddScoped<ISorter<User, UserSortField>, UserSorter>();
+
+        return services;
+    }
+
+    public static IServiceCollection InjectAutoMapper(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+}
