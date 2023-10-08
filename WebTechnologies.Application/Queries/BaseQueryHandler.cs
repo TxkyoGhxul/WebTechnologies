0248fd3ffee05ea55ff023b77d6a5ab49b8b549c
pxmiu <pmnsplay@gmail.com>
2023-10-08 16:04:47 +0300

    Added base query handler

---
+++ WebTechnologies.Application/Queries/BaseQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿using WebTechnologies.Application.Interfaces;
+using WebTechnologies.Domain.Models.Base;
+
+namespace WebTechnologies.Application.Queries;
+
+public abstract class BaseQueryHandler<TModel> where TModel : Entity, new()
+{
+    protected readonly IRepository<TModel> _repository;
+
+    protected BaseQueryHandler(IRepository<TModel> repository)
+    {
+        _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+    }
+}
