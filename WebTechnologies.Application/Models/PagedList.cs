f87b09464d66110237e30a140f8d4f807cacedbd
pxmiu <pmnsplay@gmail.com>
2023-10-08 16:03:38 +0300

    Added validation

--- WebTechnologies.Application/Models/PagedList.cs
+++ WebTechnologies.Application/Models/PagedList.cs
@@ -3,6 +3,9 @@
 namespace WebTechnologies.Application.Models;
 public class PagedList<T>
 {
+    private const int _defaultPageSize = 10;
+    private const int _defaultPageNumber = 10;
+
     public IReadOnlyCollection<T> Items { get; }
     public int PageNumber { get; }
     public int TotalPages { get; }
@@ -22,9 +25,28 @@ public class PagedList<T>
 
     public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize, CancellationToken cancellationToken = default)
     {
+        SetDefaultPageSizeIfNonPositive(ref pageSize);
+        SetDefaultPageNumberIfNonPositive(ref pageNumber);
+
         var count = await source.CountAsync(cancellationToken);
         var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync(cancellationToken);
 
         return new(items, count, pageNumber, pageSize);
+    }
+
+    private static void SetDefaultPageSizeIfNonPositive(ref int pageSize)
+    {
+        if (pageSize <= 0)
+        {
+            pageSize = _defaultPageSize;
+        }
+    }
+
+    private static void SetDefaultPageNumberIfNonPositive(ref int pageNumber)
+    {
+        if (pageNumber <= 0)
+        {
+            pageNumber = _defaultPageNumber;
+        }
     }
 }
