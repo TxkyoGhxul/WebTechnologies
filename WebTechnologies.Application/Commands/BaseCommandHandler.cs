c87d5d90bb4abe15b1b87fa07520194d4d9dc63f
pxmiu <pmnsplay@gmail.com>
2023-10-08 16:04:31 +0300

    Added base command handler

---
+++ WebTechnologies.Application/Commands/BaseCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using WebTechnologies.Application.Interfaces;
+using WebTechnologies.Domain.Models.Base;
+
+namespace WebTechnologies.Application.Commands;
+public abstract class BaseCommandHandler<TModel> where TModel : Entity, new()
+{
+    protected readonly IRepository<TModel> _repository;
+    protected readonly IUnitOfWork _unitOfWork;
+
+    protected BaseCommandHandler(IRepository<TModel> repository, IUnitOfWork unitOfWork)
+    {
+        _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+    }
+}
