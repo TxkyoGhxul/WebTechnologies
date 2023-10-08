06b5cf8c59fd8c1a91332bc1e01af32d7828a3b4
pxmiu <pmnsplay@gmail.com>
2023-10-08 16:04:01 +0300

    Added mapper

---
+++ WebTechnologies.Application/Mappers/UserMapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using WebTechnologies.Application.Queries.UserQueries.GetSingle;
+using WebTechnologies.Domain.Models;
+
+namespace WebTechnologies.Application.Mappers;
+internal class UserMapperProfile : Profile
+{
+    public UserMapperProfile()
+    {
+        CreateMap<User, SingleUserResponse>();
+    }
+}
