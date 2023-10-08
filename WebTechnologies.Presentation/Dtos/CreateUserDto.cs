d2bffe2d4d05c76735de9e4370b3169dbe76f28e
pxmiu <pmnsplay@gmail.com>
2023-10-09 01:18:26 +0300

    User dtos

--- WebTechnologies.Presentation/Dtos/CreateUserDto.cs
+++ WebTechnologies.Presentation/Dtos/CreateUserDto.cs
@@ -1,5 +1,2 @@
 ﻿namespace WebTechnologies.Presentation.Dtos;
 public record CreateUserDto(string Name, string Email, DateOnly BirthDate);
-public record DeleteUserDto(Guid UserId);
-public record UpdateUserDto(Guid UserId, string Name, string Email, DateOnly BirthDate);
-public record AddUserRoleDto(Guid UserId, Guid RoleId);
