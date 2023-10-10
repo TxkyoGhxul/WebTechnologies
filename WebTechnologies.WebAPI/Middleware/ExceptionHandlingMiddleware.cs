6dbe14864a4ad32db47197495137471a962229db
pxmiu <pmnsplay@gmail.com>
2023-10-10 11:44:52 +0300

    Added middlewares

---
+++ WebTechnologies.WebAPI/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using FluentValidation;
+using System.Net;
+using System.Text.Json;
+
+namespace WebTechnologies.WebAPI.Middleware;
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next) => _next = next;
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (ValidationException ex)
+        {
+            await HandleExceptionAsync(httpContext, ex.Message,
+                HttpStatusCode.BadRequest, "ValidationContext error!");
+        }
+        catch (ArgumentNullException ex)
+        {
+            await HandleExceptionAsync(httpContext, ex.Message,
+                HttpStatusCode.BadRequest, "Argument cant be null!");
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(httpContext, ex.Message,
+                HttpStatusCode.InternalServerError, "Something went wrong!");
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext httpContext,
+        string exceptionMessage, HttpStatusCode statusCode, string message)
+    {
+        var response = httpContext.Response;
+
+        response.ContentType = "application/json";
+        response.StatusCode = (int)statusCode;
+
+        var error = new
+        {
+            Message = message,
+            ExceptionMessage = exceptionMessage,
+            StatusCode = statusCode
+        };
+
+        string result = JsonSerializer.Serialize(error);
+
+        await response.WriteAsJsonAsync(result);
+
+        httpContext.Response.StatusCode = (int)statusCode;
+        await httpContext.Response.WriteAsync(message);
+    }
+}
---
+++ WebTechnologies.WebAPI/Middleware/LoggingMiddleware.cs
@@ -0,0 +1,22 @@
+﻿namespace WebTechnologies.WebAPI.Middleware;
+
+public class LoggingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<LoggingMiddleware> _logger;
+
+    public LoggingMiddleware(RequestDelegate next, ILogger<LoggingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        _logger.LogInformation("Executing on path: {@Path}", httpContext.Request.Path);
+
+        await _next(httpContext);
+
+        _logger.LogInformation("Executed on path: {@Path}", httpContext.Request.Path);
+    }
+}
